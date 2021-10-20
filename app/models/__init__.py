fb1b3519eb70e1699ddd8a21db4266aae62bfe8f
Arnold C <acabang23@gmail.com>
2021-10-20 12:36:24 -0700

    set up migrations

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,7 @@
 from .db import db
 from .user import User
+from .avatars import Avatar
+from .genres import Genre
+from .profiles import Profile
+from .shows import Show
+from .watchlists import Watchlist
