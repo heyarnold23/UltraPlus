0708a836116c07c9ee745961e0c1c83ea8d15258
Arnold C <acabang23@gmail.com>
2021-10-23 15:33:30 -0700

    made ultra plus pic on loginform a navlink to home page and changed background color of page

--- react-app/src/components/LoginPage/index.js
+++ react-app/src/components/LoginPage/index.js
@@ -1,11 +1,15 @@
+import { NavLink } from 'react-router-dom';
 import LoginForm from '../auth/LoginForm'
 import styles from './LoginPage.module.css'
+
 
 export default function LoginPage() {
     return (
         <>
             <div id={styles.page}>
+                <NavLink to='/'>
                 <div id={styles.picDiv} />
+                </NavLink>
                 <LoginForm />
             </div>
         </>
