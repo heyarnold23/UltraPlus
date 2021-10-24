273019b011251d8084c5aebb1845a37008e80470
Arnold C <acabang23@gmail.com>
2021-10-24 14:09:50 -0700

    added genre seeders

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -2,6 +2,7 @@ from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .avatars import seed_avatars, undo_avatars
 from .profiles import seed_profiles, undo_profiles
+from .genres import seed_genres, undo_genres
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -14,6 +15,7 @@ def seed():
     seed_users()
     seed_avatars()
     seed_profiles()
+    seed_genres()
     # Add other seed functions here
 
 
@@ -23,4 +25,5 @@ def undo():
     undo_users()
     undo_avatars()
     undo_profiles()
+    undo_genres()
     # Add other undo functions here
