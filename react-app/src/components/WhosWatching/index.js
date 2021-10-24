34a14e40dcd45380cda22342f5d8cee79d9309f7
Arnold C <acabang23@gmail.com>
2021-10-24 12:10:11 -0700

    touched up css on whos watching page for the avatars and edit and delete buttons

--- react-app/src/components/WhosWatching/index.js
+++ react-app/src/components/WhosWatching/index.js
@@ -82,10 +82,10 @@ export default function WhosWatching() {
                                                 <div id={styles.profilePic} style={{ backgroundImage: `url(${profile.avatar_pic.image_url})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>
                                                     <div id={styles.buttonDiv}>
                                                         <NavLink to={{ pathname: '/edit-profile', state: { avatar_id: `${profile.avatar_pic_id}`, avatar_url: `${profile.avatar_pic.image_url}`, profile_name: `${profile.name}`, profile_id: `${profile.id}` } }}>
-                                                            <div id={styles.editDeleteButton}><MdOutlineEditLocationAlt /></div>
+                                                            <div id={styles.editButton}><MdOutlineEditLocationAlt /></div>
                                                             {profile.avatar_id}
                                                         </NavLink>
-                                                        <div id={styles.editDeleteButton} onClick={(e) => { modal(e, profile?.id, profile.name) }}><TiDeleteOutline /></div>
+                                                        <div id={styles.deleteButton} onClick={(e) => { modal(e, profile?.id, profile.name) }}><TiDeleteOutline /></div>
                                                     </div>
                                                 </div>
                                             </>
