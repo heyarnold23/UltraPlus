4cffccfe0a35466380f75eaad88121e7b4ad24cf
Arnold C <acabang23@gmail.com>
2021-10-25 19:41:29 -0700

    added backend routes for watchlists

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.profile_routes import profile_routes
 from .api.show_routes import show_routes
+from .api.watchlist_routes import watchlist_routes
 
 from .seeds import seed_commands
 
@@ -35,6 +36,7 @@ app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(profile_routes, url_prefix='/api/profiles')
 app.register_blueprint(show_routes, url_prefix='/api/shows')
+app.register_blueprint(watchlist_routes, url_prefix='/api/watchlists')
 db.init_app(app)
 Migrate(app, db)
 
---
+++ app/api/watchlist_routes.py
@@ -0,0 +1,58 @@
+from flask import Blueprint, jsonify, request
+from flask_login import login_required
+from app.forms.watchlist_form import WatchlistForm
+from app.models import Watchlist, db
+from app.api.auth_routes import validation_errors_to_error_messages
+
+
+watchlist_routes = Blueprint('watchlists', __name__)
+
+@watchlist_routes.route('/<int:id>')
+def watchlists_by_profile_id(id):
+    watchlists = Watchlist.query.filter(
+        Watchlist.profile_id == id
+    )
+    return {watchlist.id:watchlist.to_dict() for watchlist in watchlists}
+
+@watchlist_routes.route('', methods=["POST"])
+@login_required
+def postWatchlist():
+    form = WatchlistForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        watchlist = Watchlist(
+            name=form.data['name'],
+            profile_id=form.data['user_id']
+        )
+        db.session.add(watchlist)
+        db.session.commit()
+        return watchlist.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+@watchlist_routes.route('/edit/<int:id>', methods=["PUT"])
+@login_required
+def update_watchlist(id):
+    # print("inside api routeee id", id)
+    watchlist = Watchlist.query.get(id)
+    got_name = request.json.get('name', watchlist.name)
+    if len(got_name) < 1 or len(got_name) > 50 or got_name.isspace():
+        return {'errors': 'Must be between 1 to 50 characters'}, 401
+    watchlist.name = request.json.get('name', watchlist.name)
+    db.session.commit()
+    return watchlist.to_dict()
+
+@watchlist_routes.route('/delete/<int:id>')
+@login_required
+def delete_watchlist(id):
+    watchlist = Watchlist.query.get(id)
+    db.session.delete(watchlist)
+    db.session.commit()
+    return {
+        'deleted_watchlist': watchlist.to_dict()
+    }
+
+# @comment_routes.route('')
+# def comments():
+#     print("Comments hitting hard")
+#     comments = Comment.query.all()
+#     return {comment.id:comment.to_dict() for comment in comments}
