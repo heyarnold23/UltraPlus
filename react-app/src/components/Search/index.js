e2e93a4b84326c95cdd3ecf52e636c929aa7584c
Arnold C <acabang23@gmail.com>
2021-11-30 14:49:13 -0800

    set up front end layout for search

--- react-app/src/components/Search/index.js
+++ react-app/src/components/Search/index.js
@@ -1,11 +1,39 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-
+import styles from './Search.module.css'
 
 export default function Search() {
 
+    // const [searchInput, setSearchInput] = useState();
+    const [searchResults, setSearchResults] = useState();
 
-    return(
-        <div>Hello</div>
-    )
+    const sendSearch = (e) => {
+        console.log(e.target.value);
+        const searchInput = e.target.value;
+
+        // assign to variable: await dispatch thunk action for searching
+        // console.log the variable
+
+    }
+
+    return (
+        <>
+            <div id={styles.page}>
+                <div id={styles.midContainer}>
+                    <div id={styles.searchDiv}>
+                        <form action="/search" method="get">
+                            <input
+                                type="search"
+                                name="searchVal"
+                                id="searchBar"
+                                placeholder="Search by title..."
+                                onChange={sendSearch}
+                            />
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
