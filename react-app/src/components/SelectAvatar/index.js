0f53af5faa4002c0625e9f719981f6206e9cae38
Arnold C <acabang23@gmail.com>
2022-01-05 13:32:43 -0800

    clean up select avatar component

--- react-app/src/components/SelectAvatar/index.js
+++ react-app/src/components/SelectAvatar/index.js
@@ -6,31 +6,24 @@ import { getAvatarsThunk } from '../../store/avatars'
 import styles from './SelectAvatar.module.css'
 
 export default function SelectAvatar() {
-    const sessionUser = useSelector(state => state.session.user);
     const dispatch = useDispatch();
     let location = useLocation();
     const fromEdit = location?.state?.fromEditProfile
     const profileName = location?.state?.profileName
     const profileId = location?.state?.profileId
 
-    // console.log('\n\n\n\n\n\n FROM EDITTTT', fromEdit);
-    // console.log('\n\n\n\n\n\n FROM profileName', profileName);
-    // console.log('\n\n\n\n\n\n FROM profileId', profileId);
     useEffect(() => {
         dispatch(getAvatarsThunk())
     }, [dispatch])
 
     const avatars = useSelector(store => store?.avatars)
     const avatarsArr = Object.values(avatars)
-    // console.log("\n\n\n\n\n\n avatars", avatarsArr, "\n\n\n\n\n\n\n\n\n\n");
+
 
     return (
         <>
             <div id={styles.navContainer}>
                 <div id={styles.navPic} />
-                {/* <NavLink id={styles.skipButton} to='/edit-profile'>
-                    <span id={styles.skip}>SKIP</span>
-                </NavLink> */}
             </div>
             <div id={styles.page}>
                 <div id={styles.chooseDiv}>
@@ -38,7 +31,6 @@ export default function SelectAvatar() {
                 </div>
                 {/* here, do mappings for avatars*/}
                 <div id={styles.avatarContainer}>
-                    {/* <div id={styles.avatarRow}> */}
                     {avatarsArr?.map((avatar) => {
                         return (
                             <>
@@ -58,7 +50,6 @@ export default function SelectAvatar() {
                         )
                     }
                     )}
-                    {/* </div> */}
                 </div>
             </div>
 
